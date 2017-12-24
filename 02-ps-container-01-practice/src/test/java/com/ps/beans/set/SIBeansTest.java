5e0bfeda0368770870a7bbe88d1ea6433b5d4646
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-24 03:26:33 +0300

    02-ps-cont-01/ SIbean test completed

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
@@ -1,5 +1,6 @@
 package com.ps.beans.set;
 
+import com.ps.beans.SimpleBeanImpl;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,7 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by iuliana.cosmina on 3/26/16.
@@ -26,5 +26,13 @@ public class SIBeansTest {
         }
 
         //TODO 4. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
+        ComplexBeanImpl complexBean = (ComplexBeanImpl) ctx.getBean("complexBean0");
+        assertNotNull(complexBean);
+        SimpleBeanImpl simpleBean1 = (SimpleBeanImpl) complexBean.getSimpleBean();
+        assertNotNull(simpleBean1);
+        ComplexBeanImpl complexBean1 = (ComplexBeanImpl) ctx.getBean("complexBean1");
+        assertNotNull(complexBean1);
+        ComplexBean2Impl complexBean2 = (ComplexBean2Impl) ctx.getBean("complexBean2");
+        assertNotNull(complexBean2);
     }
 }
