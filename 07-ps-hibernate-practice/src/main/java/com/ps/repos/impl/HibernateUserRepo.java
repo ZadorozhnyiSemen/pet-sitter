1fcf56497a3b87373030acd65544366d0fcbbd8f
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-24 04:28:19 +0300

    07-ps-hibernate-practice/ User repo implemented

--- 07-ps-hibernate-practice/src/main/java/com/ps/repos/impl/HibernateUserRepo.java
+++ 07-ps-hibernate-practice/src/main/java/com/ps/repos/impl/HibernateUserRepo.java
@@ -7,7 +7,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -52,7 +51,8 @@ public class HibernateUserRepo implements UserRepo {
     @Override
     public List<User> findAllByUserName(String username, boolean exactMatch) {
         if (exactMatch) {
-            return new ArrayList<>();  // TODO 36. Add Hibernate query to extract wll users with username = :username
+            return session().createQuery("from User u where username = ?")
+                    .setParameter("username", username).list();
         } else {
             return session().createQuery("from User u where username like ?")
                     .setParameter(0, "%" + username + "%").list();
@@ -67,7 +67,7 @@ public class HibernateUserRepo implements UserRepo {
 
     @Override
     public long countUsers() {
-        return 0L; // TODO 37. Add query to count all users
+        return (long) session().createQuery("select count(u) from User u").uniqueResult();
     }
 
     @Override
@@ -88,7 +88,7 @@ public class HibernateUserRepo implements UserRepo {
 
     @Override
     public void deleteById(Long userId) {
-        // TODO 38. Add code to delete an user by its id.
+        session().createQuery("delete from User u where u.id = :id").setParameter("id", userId);
     }
 
     @Override
