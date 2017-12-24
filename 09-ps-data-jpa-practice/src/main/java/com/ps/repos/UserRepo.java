e4b028da52303856001840fa587e63b44070bedd
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-24 17:15:43 +0300

    09-ps-data-jpa-practice/ repo completed

--- 09-ps-data-jpa-practice/src/main/java/com/ps/repos/UserRepo.java
+++ 09-ps-data-jpa-practice/src/main/java/com/ps/repos/UserRepo.java
@@ -2,23 +2,28 @@ package com.ps.repos;
 
 import com.ps.ents.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created by iuliana.cosmina on 2/23/16.
  */
-//TODO 44. Complete the definition of this interface to make the tests in TestUserRepo.java pass.
+@Transactional
 public interface UserRepo extends JpaRepository<User, Long> {
 
-
+    @Query("select u from User u where u.username = ?1")
     List<User> findAllByUserName(String username);
 
+    @Query("select u from User u where u.username = :un")
     User findOneByUsername(@Param("un") String username);
 
+    @Query("select u.username from User u where u.id = ?1")
     String findUsernameById(Long id);
 
+    @Query("select count(u) from User u")
     long countUsers();
 
 }
