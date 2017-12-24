0ffb7c9b31d87dddb06c4b581193fd73f179d2d2
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-24 04:13:46 +0300

    04-ps-jdbc-practice/ bonus ignored

--- 05-ps-jdbc-practice/src/test/java/com/ps/repo/TestNamedJdbcTemplateUserRepo.java
+++ 05-ps-jdbc-practice/src/test/java/com/ps/repo/TestNamedJdbcTemplateUserRepo.java
@@ -9,13 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -26,7 +22,6 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestDataConfig.class, AppConfig.class})
 @ActiveProfiles("dev")
-// TODO 30. [BONUS] Write test methods to cover all methods in JdbcNamedTemplateUserRepo
 public class TestNamedJdbcTemplateUserRepo {
 
     @Autowired
